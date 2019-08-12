c81b6f61ea11149e8379337a881b1a5d36e9b6ee
zouhanzhang666 <zouhanzhang666@163.com>
2019-08-12 22:40:31 +0800

    8-12

--- testapp1/models.py
+++ testapp1/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Classes(models.Model):#对应表
+class Classes(models.Model):#对应数据库中的表
     class_name=models.CharField(max_length=20)
     stu_number=models.IntegerField()
     isDelete=models.BooleanField()
