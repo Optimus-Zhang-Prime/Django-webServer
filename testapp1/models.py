4a25f3c763cdd4a1bcaa33002f53f278046086b3
zouhanzhang666 <zouhanzhang666@163.com>
2019-08-12 18:37:31 +0800

    8-12

--- testapp1/models.py
+++ testapp1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Classes(models.Model):#对应表
+    class_name=models.CharField(max_length=20)
+    stu_number=models.IntegerField()
+    isDelete=models.BooleanField()
+class Students(models.Model):
+    name=models.CharField(max_length=20)
+    gender=models.CharField(max_length=4)
+    stu_class=models.ForeignKey("Classes",on_delete=models.CASCADE)#关联外键
