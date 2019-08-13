92556f3ebb4801f78191f799a210fccb60f3e942
zouhanzhang666 <zouhanzhang666@163.com>
2019-08-13 22:18:13 +0800

    8-12

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -111,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'#自己改的
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'#自己改的
 
 USE_I18N = True
 
