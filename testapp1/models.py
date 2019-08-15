32ae499789061c7b2e9535efa01e2ee72743d253
zouhanzhang666 <zouhanzhang666@163.com>
2019-08-15 21:19:21 +0800

    8-12

--- testapp1/models.py
+++ testapp1/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
-class Classes(models.Model):#对应数据库中的表
-    class_name=models.CharField(max_length=20)
-    stu_number=models.IntegerField()
-    isDelete=models.BooleanField()
+class Classes(models.Model):  # 对应数据库中的表
+    class_name = models.CharField(max_length=20)
+    stu_number = models.IntegerField()
+    isDelete = models.BooleanField()
+
+    def __str__(self):
+        return self.class_name
+
+
 class Students(models.Model):
-    name=models.CharField(max_length=20)
-    gender=models.CharField(max_length=4)
-    stu_class=models.ForeignKey("Classes",on_delete=models.CASCADE)#关联外键
+    name = models.CharField(max_length=20)
+    gender = models.CharField(max_length=4)
+    stu_class = models.ForeignKey("Classes", on_delete=models.CASCADE)  # 关联外键
+    isDelete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
