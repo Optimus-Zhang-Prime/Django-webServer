a499b8b4974a6c0b8bf77edae454779fa88a2025
zouhanzhang666 <zouhanzhang666@163.com>
2019-08-17 23:02:05 +0800

    8-12

--- testapp1/models.py
+++ testapp1/models.py
@@ -1,15 +1,31 @@
 from django.db import models
+
 '''
 关于models：
 TextField：大文本字段
 DecimalField(max_digits=总位数，decimal_places=小数点后位数):小数
+DateField(auto_now=True记录最后一次修改,auto_now_add=True记录创建时间)
+FileField文件
+ImageField图片
+
+字段选项：
+db_column=''指定字段名，默认为属性名
+db_index=True 以此为索引
+primary_key=True 以此为主键
+unique=True 必须唯一
+
+关系：
+ForeignKey：一对多，将字段放在多的端中
+ManyToManyField：多对多，放两端
+OneToOneField：一对一，放任一一端
 '''
+
 
 # Create your models here.
 class Classes(models.Model):  # 一个类对应数据库中的一个表
-    class_name = models.CharField(max_length=20)#字符串
+    class_name = models.CharField(max_length=20)  # 字符串
     stu_number = models.IntegerField()
-    isDelete = models.BooleanField()
+    isDelete = models.BooleanField()  # 可设置default
 
     def __str__(self):
         return self.class_name
