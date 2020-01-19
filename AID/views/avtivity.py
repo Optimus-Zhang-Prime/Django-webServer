3bc163c63e3667ebca3bc5c54de866af2049fdae
zouhanzhang666 <zouhanzhang666@163.com>
2020-01-19 21:44:14 +0800

    update

--- AID/views/avtivity.py
+++ AID/views/avtivity.py
@@ -26,36 +26,83 @@ def seeActivity(request):  # 显示培训和招募活动
     return render(request, "SeeActivity.html", locals())
 
 
+def myActivity(request):  # 已报名的活动
+    username = request.user.username
+    try:
+        trainlist = models.UserSignupTrainList.objects.get(User=request.user)
+        trainActivity = trainlist.TrainActivity.all()
+    except:
+        messages.add_message(request, messages.INFO, "您还没有报名培训活动")
+    try:
+        recruitlist = models.UserSignupRecruitList.objects.get(User=request.user)
+        recruitActivity = recruitlist.RecruitActivity.all()
+    except:
+        messages.add_message(request, messages.INFO, "您还没有报名招募活动")
+    return render(request, "myActivity.html", locals())
+
+
 @login_required()
 def manageTrainActivity(request, id):  # 培训活动发布者管理报名人员
     trainActivity = models.TrainActivity.objects.get(pk=id)
-    if request.user.is_authenticated:
-        username = request.user.username
-        manager = User.objects.filter(trainactivity=trainActivity)[0]
-        if request.user.type == 1 and manager.username == username:
-            list = models.TrainSignupUserList.objects.get(Activity=trainActivity)
-            allsignupUserlist = User.objects.filter(trainsignupuserlist=list)
-        else:
-            messages.add_message(request, messages.INFO, "您没有权限对该活动进行管理")
+    username = request.user.username
+    manager = User.objects.filter(trainactivity=trainActivity)[0]
+    if request.user.type == 1 and manager.username == username:  # 判断是否是该活动的发起者
+        list = models.TrainSignupUserList.objects.get(Activity=trainActivity)
+        userlist=list.SignUser.all()
+        #allsignupUserlist = User.objects.filter(trainsignupuserlist=list)
+    else:
+        messages.add_message(request, messages.INFO, "您没有权限对该活动进行管理")
     return render(request, "manageTrainActivity.html", locals())
 
 
 @login_required()
 def manageRecruitActivity(request, id):  # 招募活动发布者管理报名人员
     recruitActivity = models.RecruitActivity.objects.get(pk=id)
-    if request.user.is_authenticated:
-        username = request.user.username
-        manager = User.objects.filter(recruitactivity=recruitActivity)[0]
-        if request.user.type == 2 and manager.username == username:
-            list = models.RecruitSignupUserList.objects.get(Activity=recruitActivity)
-            allsignupUserlist = User.objects.filter(recruitsignupuserlist=list)
-        else:
-            messages.add_message(request, messages.INFO, "您没有权限对该活动进行管理")
+    username = request.user.username
+    manager = User.objects.filter(recruitactivity=recruitActivity)[0]
+    if request.user.type == 2 and manager.username == username:
+        list = models.RecruitSignupUserList.objects.get(Activity=recruitActivity)
+        allsignupUserlist = User.objects.filter(recruitsignupuserlist=list)
+    else:
+        messages.add_message(request, messages.INFO, "您没有权限对该活动进行管理")
     return render(request, "manageTrainActivity.html", locals())
+
+
+def trainRatify(request, actID, userID):
+    try:
+        joinUser = User.objects.get(pk=userID)
+        activity = models.TrainActivity.objects.get(pk=actID)
+        try:
+            joinedList = models.TrainJoinedUserList.objects.get(Activity=activity)
+        except:
+            joinedList = models.TrainJoinedUserList.objects.create(Activity=activity)
+        joinedList.SignUser.add(joinUser)
+        messages.add_message(request, messages.INFO, "成功")
+        return HttpResponseRedirect("../../../aid/seeActivity/")
+    except:
+        messages.add_message(request, messages.INFO, "失败")
+        return HttpResponseRedirect("../../../aid/seeActivity/")
+
+
+def recruitRatify(request, actID, userID):
+    try:
+        joinUser = User.objects.get(pk=userID)
+        activity = models.RecruitActivity.objects.get(pk=actID)
+        try:
+            joinedList = models.RecruitJoinedUserList.objects.get(Activity=activity)
+        except:
+            joinedList = models.RecruitJoinedUserList.objects.create(Activity=activity)
+        joinedList.SignUser.add(joinUser)
+        messages.add_message(request, messages.INFO, "成功")
+        return HttpResponseRedirect("../../../aid/seeActivity/")
+    except:
+        messages.add_message(request, messages.INFO, "失败")
+        return HttpResponseRedirect("../../../aid/seeActivity/")
 
 
 @login_required()
 def signUpTrain(request, id):  # 志愿者报名培训活动
+    username = request.user.username
     try:
         trainActivity = models.TrainActivity.objects.get(pk=id)
         Trainsignuplist = models.TrainSignupUserList.objects.get(Activity=trainActivity)
@@ -73,6 +120,7 @@ def signUpTrain(request, id):  # 志愿者报名培训活动
 
 @login_required()
 def signUpRecruit(request, id):
+    username = request.user.username
     try:
         recruitActivity = models.RecruitActivity.objects.get(pk=id)
         Recruitsignuplist = models.RecruitSignupUserList.objects.get(Activity=recruitActivity)
@@ -91,6 +139,7 @@ def signUpRecruit(request, id):
 
 @login_required(login_url='/login/')
 def writeTrainActivity(request):
+    username = request.user.username
     messages.get_messages(request)
     if request.method == 'POST':
         user = request.user
@@ -115,6 +164,7 @@ def writeTrainActivity(request):
 
 @login_required(login_url='/login/')
 def writeRecruitActivity(request):
+    username = request.user.username
     messages.get_messages(request)
     if request.method == 'POST':
         user = request.user
