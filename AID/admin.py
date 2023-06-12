9361c75ef499a47c16409334ec39c2d10b98578a
邹汉章 <45626756+Optimus-Zhang-Prime@users.noreply.github.com>
2023-06-12 13:39:12 +0800

    Update admin.py

--- AID/admin.py
+++ AID/admin.py
@@ -6,7 +6,7 @@ from users.models import User
 # Register your models here.
 class TrainActivityAdmin(admin.ModelAdmin):
     list_display = ( 'title', 'description', 'createDate', 'enable', 'isAttest')
-
+#
 
 admin.site.register(TrainActivity, TrainActivityAdmin)
 #admin.site.register(User, UserAdmin)
